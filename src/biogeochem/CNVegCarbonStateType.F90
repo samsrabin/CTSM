6c74360af5597a974be2c08134dfcb2f838de7e0
OlgaDom <olga.dombrowski@gmail.com>
2022-11-15 16:15:53 +0100

    cleaned up some scripts, added some crop types to active CFTs

--- src/biogeochem/CNVegCarbonStateType.F90
+++ src/biogeochem/CNVegCarbonStateType.F90
@@ -2606,7 +2606,6 @@ contains
             soilbiogeochem_totlitc_col(c)   + &
             soilbiogeochem_totsomc_col(c)   + &
             soilbiogeochem_ctrunc_col(c)
-
     end do
 
   end subroutine Summary_carbonstate
