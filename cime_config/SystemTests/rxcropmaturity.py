3b6b212632f01b1beb629acd5eec4d377a750fcb
Sam Rabin <sam.rabin@gmail.com>
2023-07-23 09:30:12 -0600

    Rolling back to known good shdates files.

--- cime_config/SystemTests/rxcropmaturity.py
+++ cime_config/SystemTests/rxcropmaturity.py
@@ -181,23 +181,23 @@ class RXCROPMATURITY(SystemTestsCommon):
         # Eventually, I want to remove these hard-coded resolutions so that this test can generate
         # its own sowing and harvest date files at whatever resolution is requested.
         lnd_grid = self._case.get_value("LND_GRID")
-        #input_data_root = self._case.get_value("DIN_LOC_ROOT")
-        #processed_crop_dates_dir = f"{input_data_root}/lnd/clm2/cropdata/calendars/processed"
-        processed_crop_dates_dir = "/glade/work/samrabin/crop_dates"
+        input_data_root = self._case.get_value("DIN_LOC_ROOT")
+        processed_crop_dates_dir = f"{input_data_root}/lnd/clm2/cropdata/calendars/processed"
+        #processed_crop_dates_dir = "/glade/work/samrabin/crop_dates"
         if lnd_grid == "10x15":
             self._sdatefile = os.path.join(
                 processed_crop_dates_dir,
-                #"sdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230330_165301.nc",
+                "sdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230330_165301.nc",
                 #"minimal_crop_dates/sdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230721_165017.nc",
                 #"minimal_sdate_file/sdates.min0.nc",
-                "sdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230721_144617.nc",
+                #"sdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230721_144617.nc",
             )
             self._hdatefile = os.path.join(
                 processed_crop_dates_dir,
-                #"hdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230330_165301.nc",
+                "hdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230330_165301.nc",
                 #"minimal_crop_dates/hdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230721_165017.nc",
                 #"minimal_sdate_file/hdates.min0.nc",
-                "hdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230721_144617.nc",
+                #"hdates_ggcmi_crop_calendar_phase3_v1.01_nninterp-f10_f10_mg37.2000-2000.20230721_144617.nc",
             )
         elif lnd_grid == "1.9x2.5":
             self._sdatefile = os.path.join(
