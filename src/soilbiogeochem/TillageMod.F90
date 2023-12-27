c16c00323b4af2d08da66847a0aedf09cd3c0d37
Sam Rabin <sam.rabin@gmail.com>
2023-12-27 11:30:25 -0700

    Added more printout to readParams_netcdf().

--- src/soilbiogeochem/TillageMod.F90
+++ src/soilbiogeochem/TillageMod.F90
@@ -160,6 +160,16 @@ contains
 
     ! Troubleshooting
     if (masterproc) then
+        write(iulog, *) 'tillage_intensity = ',tillage_intensity
+        write(iulog, *) 'ndecomp_pools = ',ndecomp_pools
+        write(iulog, *) 'shape(tempr) = ',shape(tempr)
+        do p = 1, ndecomp_pools
+            do s = 1, ntill_stages_max
+                write(iulog, '(A5,I2,A7,I2,A16,F5.3)') 'pool ',p,' stage ',s,' tillage_mult = ',tempr(tillage_intensity,p,s)
+            end do
+        end do
+        write(iulog, *) 'shape(tempr(tillage_intensity,1:ndecomp_pools,:)) = ',shape(tempr(tillage_intensity,1:ndecomp_pools,:))
+        write(iulog, *) 'shape(tillage_mults_allphases) = ',shape(tillage_mults_allphases)
         do p = 1, ndecomp_pools
             do s = 1, ntill_stages_max
                 write(iulog, '(A5,I2,A7,I2,A16,F5.3)') 'pool ',p,' stage ',s,' tillage_mult = ',tillage_mults_allphases(p,s)
