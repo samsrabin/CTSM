4f8b424293dc0804bb7f2a53d1517fcbc8563cf3
Sam Rabin <sam.rabin@gmail.com>
2024-06-11 14:52:38 -0600

    Final cleanup.

--- src/main/controlMod.F90
+++ src/main/controlMod.F90
@@ -554,6 +554,7 @@ contains
     ! ----------------------------------------------------------------------
     ! Read in other namelists for other modules
     ! ----------------------------------------------------------------------
+
     call mpi_bcast (use_init_interp, 1, MPI_LOGICAL, 0, mpicom, ierr)
     if (use_init_interp) then
        call initInterp_readnl( NLFilename )
@@ -563,6 +564,7 @@ contains
     !For future version, I suggest to  put the following two calls inside their
     !own modules, which are called from their own initializing methods
     call init_hydrology( NLFilename )
+
     call soil_resistance_readnl ( NLFilename )
     call CanopyFluxesReadNML    ( NLFilename )
     call CanopyHydrology_readnl ( NLFilename )
@@ -575,6 +577,7 @@ contains
     ! ----------------------------------------------------------------------
     ! Broadcast all control information if appropriate
     ! ----------------------------------------------------------------------
+
     call control_spmd()
 
     ! ----------------------------------------------------------------------
@@ -1195,7 +1198,7 @@ contains
     end if
   end subroutine control_print
 
-  
+
   !-----------------------------------------------------------------------
   subroutine check_missing_initdata_status(finidat_interp_dest)
    !
@@ -1251,8 +1254,6 @@ contains
     logical                    :: lexists
     integer                    :: ncid
     character(len=SHR_KIND_CL) :: initial_source_file
-    integer                    :: klen
-    character(len=SHR_KIND_CL) :: status_file
     integer                    :: status
     character(len=*), parameter :: subname = 'apply_use_init_interp'
     !-----------------------------------------------------------------------
@@ -1281,8 +1282,9 @@ contains
     inquire(file=trim(finidat_interp_dest), exist=lexists)
     if (lexists) then
 
-      ! Check that the status file also exists (i.e., that finidat_interp_dest was written successfully)
-      call check_missing_initdata_status(finidat_interp_dest)
+       ! Check that the status file also exists (i.e., that finidat_interp_dest was written successfully)
+       call check_missing_initdata_status(finidat_interp_dest)
+
        ! open the input file and check for the name of the input source file
        status = nf90_open(trim(finidat_interp_dest), 0, ncid)
        if (status /= nf90_noerr) call handle_err(status)
