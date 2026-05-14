65fd42a26bd6aa38f533b3f9608556c69f262220
Sam Rabin <samrabin@ucar.edu>
2026-05-14 17:24:01 -0600

    Add TestPftToColumn

--- python/ctsm/test/test_unit_aggregate_spatial_units.py
+++ python/ctsm/test/test_unit_aggregate_spatial_units.py
@@ -464,3 +464,63 @@ class TestPftToGridcell:
         ds_p2g_novar = ds_p2g.drop_vars(VAR_NAME)
         result = asp.ds_aggregate(ds_p2g_novar, "pft", "gridcell")
         assert result.equals(ds_p2g_novar)
+
+
+@pytest.fixture(name="ds_p2c", scope="function")
+def fixture_ds_p2c(ds_all):
+    """Make an xarray Dataset to test pft-to-column"""
+    ds = _drop_unneeded_subunits(ds_all, asp.PFTSTRINGS, asp.COLSSTRINGS)
+    ds["pfts1d_wtcol"].values = (
+        # Column sums (lots of zeros because we can test what we need without filling out all cols):
+        #                  1, 1, 0.5,      14, 0, 0,           0.8, 0, 0,       0
+        [1 / 3, 1 / 3, 1 / 3, 1, 0.5, 8, 4, 2, 0, 0, 0.1, 0.2, 0.5, 0, 0, 0, 0, 0]
+    )
+
+    # Add our test variable
+    da = xr.DataArray(
+        # Columns:
+        #           1,  2,    3,       4,  5,  6,        7,   8,    9,         10
+        data=[9, 9, 3, 87, -1.5, 7, 4, 6, 86, 24, 1, 10, 5, 100, -999, 84, 91, 55],
+        dims=[asp.PFTSTRINGS.dim],
+    )
+    ds[VAR_NAME] = da
+
+    return ds
+
+
+class TestPftToColumn:
+    """Tests of aggregating pft to column"""
+
+    EXPECTED_DA = xr.DataArray(
+        data=[7, 87, -1.5, 6, np.nan, np.nan, 5.75, np.nan, np.nan, np.nan], dims=["column"]
+    )
+
+    CHILDSTRINGS = asp.PFTSTRINGS
+    PARENTSTRINGS = asp.COLSSTRINGS
+
+    def test_da_p2c(self, ds_p2c):
+        """Test da_aggregate() for pft to column"""
+        result = asp.da_aggregate(ds_p2c, VAR_NAME, self.CHILDSTRINGS, self.PARENTSTRINGS)
+        assert result.sizes == self.EXPECTED_DA.sizes
+        are_dataarrays_close(result, self.EXPECTED_DA, self.PARENTSTRINGS.dim)
+
+    def test_ds_p2c(self, ds_p2c):
+        """Test ds_aggregate() for pft to column"""
+
+        # Build expected Dataset
+        expected: xr.Dataset
+        expected = ds_p2c.drop_vars(
+            [x for x in ds_p2c if x.startswith(f"{self.CHILDSTRINGS.prefix}1d_")] + [VAR_NAME]
+        )
+        expected[VAR_NAME] = self.EXPECTED_DA
+
+        # Get result Dataset
+        result = asp.ds_aggregate(ds_p2c, self.CHILDSTRINGS.dim, self.PARENTSTRINGS.dim)
+
+        # Compare the affected variable
+        are_dataarrays_close(result[VAR_NAME], expected[VAR_NAME], self.PARENTSTRINGS.dim)
+
+        # Now drop it and compare the rest of the Datasets
+        expected = expected.drop_vars([VAR_NAME])
+        result = result.drop_vars([VAR_NAME])
+        assert result.equals(expected)
